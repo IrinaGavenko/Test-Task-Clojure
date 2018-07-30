e3cd87a9aa530a3202118e1685612e8385d33ded
irina <irina.gavenko@phystech.edu>
2018-07-30 23:29:11 +0300

    my-frequencies

--- src/test_task/core.clj
+++ src/test_task/core.clj
@@ -1,6 +1,13 @@
 (ns test-task.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn my-frequencies
+  [coll]
+  (reduce
+    #(update %1 %2 (fnil inc 0))
+    {}
+    coll))
+
+
+(my-frequencies [1 1 2 1 3 4 2 2 3])
+(my-frequencies '(1 1 2 3 1 2 3))
+(my-frequencies #{1 2 3 4 5 6 7})
--- test/test_task/core_test.clj
+++ test/test_task/core_test.clj
@@ -1,7 +1,12 @@
 (ns test-task.core-test
   (:require [clojure.test :refer :all]
-            [test-task.core :refer :all]))
+            [test-task.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest my-frequencies
+  (are [result coll]
+    (= result (core/my-frequencies coll))
+    {1 3, 2 3, 3 2, 4 1} [1 1 2 1 3 4 2 2 3]
+    {1 3, 2 2, 3 2} '(1 1 2 3 1 2 3)))
+
+(comment (core/my-frequencies [1 1 2 1 3 4 2 2 3])
+         (core/my-frequencies '(1 1 2 3 1 2 3)))
